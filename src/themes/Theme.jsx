68d2687b8fa36ba0e12e6768417de5c7a149d0d3
Rinaldy Adin <16521390@mahasiswa.itb.ac.id>
2022-07-29 19:58:48 +0700

    First commit

---
+++ src/themes/Theme.jsx
@@ -0,0 +1,10 @@
+const Theme = {
+    // colors: {
+    //     background: '#1C1D22',
+    //     modalBackground: '#24242E',
+    //     fontColor: '#fff',
+    // },
+    // fonts: ['Ubuntu', 'Open Sans'],
+};
+
+export default Theme;
